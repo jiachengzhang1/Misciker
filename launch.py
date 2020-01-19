105560d4fa2957ab12ad979eadc6ad8ecbc4774e
Jiacheng Zhang <jiachengzhang@dhcp-10-142-197-49.uawifi.arizona.edu>
2020-01-19 01:41:21 -0700

    remove CREDENTIALS

--- launch.py
+++ launch.py
@@ -8,7 +8,7 @@ language_code = 'en_au'
 gender = 'F'
 check_list = ['wallet','keys', 'phone', 'backpack']
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r"GoogleVision.json"
+# os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r"GoogleVision.json"
 
 user_name, check_list, language_code, gender, address = getConfig()
 
